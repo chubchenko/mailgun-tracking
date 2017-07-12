aa60a46fbdb5e6b1fdde3996740e9046c9f28c14
chubchenko <artem.chubchenko@gmail.com>
2017-07-12 23:14:55 +0300

    Setup test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 require 'rack'
+require 'simplecov'
 require 'bundler/setup'
 require 'mailgun/tracking'
 
 require_relative 'support/fixture'
 require_relative 'support/rack_helpers'
+
+SimpleCov.start
 
 Mailgun::Tracking.configure do |config|
   config.api_key = 'key-qblubkqnkdn4lfes5oscf57ryllaia42'
