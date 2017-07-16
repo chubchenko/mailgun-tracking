caa00096d962c09b2f1189987ddebf2d6013a366
chubchenko <artem.chubchenko@gmail.com>
2017-07-16 18:57:08 +0300

    Move similar code -> shared examples

--- spec/mailgun/tracking/subscriber/evented_spec.rb
+++ spec/mailgun/tracking/subscriber/evented_spec.rb
@@ -1,23 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe Mailgun::Tracking::Subscriber::Evented do
-  subject(:evented) { described_class.new('delivered', callable) }
+  subject(:subscriber) { described_class.new('delivered', callable) }
 
-  let(:callable) { proc {} }
-
-  describe '#call' do
-    let(:payload) { fixture('delivered.json') }
-
-    before { allow(callable).to receive(:call).with(payload) }
-
-    it 'calls the callable' do
-      evented.call(payload)
-
-      expect(callable).to have_received(:call).with(payload)
-    end
-  end
+  it_behaves_like :subscriber
 
   describe '#subscribed_to?' do
+    let(:callable) { proc {} }
+
     it { is_expected.to be_subscribed_to(:delivered) }
     it { is_expected.not_to be_subscribed_to(:any_event) }
   end
