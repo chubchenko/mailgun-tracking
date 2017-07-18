1e7d06c387cb92d18fea9accb209d0294d4885f7
chubchenko <artem.chubchenko@gmail.com>
2017-07-18 22:21:51 +0300

    Add documentation

--- lib/mailgun/tracking/exceptions.rb
+++ lib/mailgun/tracking/exceptions.rb
@@ -1,6 +1,9 @@
 module Mailgun
   module Tracking
+    # A general Mailgun Tracking exception.
     Error = Class.new(StandardError)
+
+    # Raised when signature is invalid.
     InvalidSignature = Class.new(Error)
   end
 end
--- lib/mailgun/tracking/subscriber.rb
+++ lib/mailgun/tracking/subscriber.rb
@@ -3,7 +3,15 @@ require 'mailgun/tracking/subscriber/evented'
 
 module Mailgun
   module Tracking
+    # Namespace for classes that wraps subscribers.
     module Subscriber
+      # Determines the type of subscription.
+      #
+      # @param name [Symbol, String] The name of event.
+      # @param callable [Proc, Class] The callable object.
+      #   The callable objects should respond to call.
+      #
+      # @return [Mailgun::Tracking::Subscriber::Evented, Mailgun::Tracking::Subscriber::AllMessages]
       def self.for(name, callable)
         if name
           Evented.new(name, callable)
