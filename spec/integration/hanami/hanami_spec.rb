2e9dd11d51a80f0575af609cf8d4038445ab93c8
Artem Chubchenko <artem.chubchenko@gmail.com>
2019-11-16 15:56:33 +0200

    Add tests with Hanami 🌸 (#73)
    * Add tests with Hanami 🌸
    
    * Remove unnecessary gemfile
    
    * Omit hanami head with Ruby 2.4

---
+++ spec/integration/hanami/hanami_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'dummy/hanami/application'
+
+RSpec.describe 'Hanami', type: :integration do
+  it_behaves_like 'acts as rack' do
+    let(:app) { Hanami.app }
+  end
+end
