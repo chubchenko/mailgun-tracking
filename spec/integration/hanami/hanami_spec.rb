3c6d4f722d3f0f74a5dabdb4d60661b0ebe74416
chubchenko <artem.chubchenko@gmail.com>
2020-01-31 18:04:05 +0200

    Update integration suites

--- spec/integration/hanami/hanami_spec.rb
+++ spec/integration/hanami/hanami_spec.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-require 'dummy/hanami/application'
+if defined?(::Hanami)
+  require 'dummy/hanami/application'
 
-RSpec.describe 'Hanami', type: :integration do
-  it_behaves_like 'acts as rack' do
-    let(:app) { Hanami.app }
+  RSpec.describe 'Hanami', type: :integration do
+    it_behaves_like 'acts as rack' do
+      let(:app) { Hanami.app }
+    end
   end
 end
