7a6080f5b1eaa94cbfdb5be82c3806a9e68d6565
chubchenko <artem.chubchenko@gmail.com>
2020-02-03 19:06:34 +0200

    Fix failed specs

--- spec/support/shared_examples/integration/acts_as_rack.rb
+++ spec/support/shared_examples/integration/acts_as_rack.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-class Delivered
-  def call(payload); end
-end
-
 RSpec.shared_examples 'acts as rack' do
   let(:app) { Dummy::Application }
   let(:payload) { fixture('delivered.json') }
-  let(:delivered) { instance_double(Delivered) }
+
+  class Delivered
+    def self.call(payload); end
+  end
 
   before do
-    allow(delivered).to receive(:call)
-    allow(Delivered).to receive(:new).and_return(delivered)
+    allow(Delivered).to receive(:call)
 
-    Mailgun::Tracking.on(:delivered, Delivered.new)
+    Mailgun::Tracking.on(:delivered, Delivered)
 
-    Mailgun::Tracking.on :bounced do |payload|
-      Delivered.new.call(payload)
+    Mailgun::Tracking.on(:bounced) do |payload|
+      Delivered.call(payload)
     end
 
-    Mailgun::Tracking.all(Delivered.new)
+    Mailgun::Tracking.all(Delivered)
   end
 
   it do
     post('/mailgun', payload.to_json, 'CONTENT_TYPE' => 'application/json')
-    expect(delivered).to have_received(:call).with(
+    expect(Delivered).to have_received(:call).with(
       payload
     ).twice
   end
