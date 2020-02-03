f7bb00ed12365a4e73793734176b1d0186d9bf4a
chubchenko <artem.chubchenko@gmail.com>
2020-02-04 00:05:49 +0200

    Fix acts as rack example

--- spec/support/shared_examples/integration/acts_as_rack.rb
+++ spec/support/shared_examples/integration/acts_as_rack.rb
@@ -3,28 +3,25 @@
 RSpec.shared_examples 'acts as rack' do
   let(:app) { Dummy::Application }
   let(:payload) { fixture('delivered.json') }
-
-  class Delivered
-    def self.call(payload); end
+  let(:delivered) do
+    Class.new { def self.call(payload); end }
   end
 
   before do
-    allow(Delivered).to receive(:call)
+    allow(delivered).to receive(:call)
 
-    Mailgun::Tracking.on(:delivered, Delivered)
+    Mailgun::Tracking.on(:delivered, delivered)
 
     Mailgun::Tracking.on(:bounced) do |payload|
-      Delivered.call(payload)
+      delivered.call(payload)
     end
 
-    Mailgun::Tracking.all(Delivered)
+    Mailgun::Tracking.all(delivered)
   end
 
   it do
     post('/mailgun', payload.to_json, 'CONTENT_TYPE' => 'application/json')
-    expect(Delivered).to have_received(:call).with(
-      payload
-    ).twice
+    expect(delivered).to have_received(:call).with(payload).twice
   end
 
   context 'when the signature comparison is unsuccessful' do
